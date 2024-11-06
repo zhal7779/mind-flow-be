a1421fa07826bce1d9f0e61fd5152d9dc472547f
minyoung <zhal7779@naver.com>
2024-11-06 15:24:42 +0900

    Feat: 특정 도메인 cors 허용 설정

--- app.ts
+++ app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("hello world");
