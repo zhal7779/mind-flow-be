7bdb09c87868d2133d7aa82301a2dbe5fac6e824
minyoung <zhal7779@naver.com>
2024-11-06 18:25:11 +0900

    Feat: cors 옵션 커스텀

--- app.ts
+++ app.ts
@@ -5,6 +5,9 @@ const app = express();
 
 const corsOptions = {
   origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
+  credentials: true, // 자격 증명 허용 (쿠키 전송 허용)
 };
 
 app.use(cors(corsOptions));
