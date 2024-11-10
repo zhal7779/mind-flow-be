6b5628ee79ce4369dc39162cb2e6683fc40a3525
Minyoung Lee <zhal7779@gmail.com>
2024-11-10 22:50:47 +0900

    Feat: DB 연결 파일

--- config/dbConfigs.ts
+++ config/dbConfigs.ts
@@ -2,18 +2,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-interface DBConfig {
+interface IdbConfig {
   host: string;
   user: string;
   password: string;
   database: string;
 }
 
-const DBConfig: DBConfig = {
+const dbConfig: IdbConfig = {
   host: process.env.DATABASE_HOST || '',
   user: process.env.DATABASE_USERNAME || '',
   password: process.env.DATABASE_PASSWORD || '',
   database: process.env.DATABASE_NAME || '',
 };
 
-export default DBConfig;
+export default dbConfig;
---
+++ utils/dbConnect.ts
@@ -0,0 +1,16 @@
+import mysql from 'mysql2';
+import dbConfig from '../config/dbConfigs';
+
+// MySQL 연결 생성
+const connection = mysql.createConnection(dbConfig);
+
+// 연결 확인
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL 연결 오류:', err.stack);
+    return;
+  }
+  console.log('MySQL 연결 성공');
+});
+
+export default connection;
