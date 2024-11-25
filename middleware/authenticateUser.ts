c2a628124071ab0a0283e0fcd8a574f9df517531
Minyoung Lee <zhal7779@gmail.com>
2024-11-26 00:15:54 +0900

    Feat: JWT 인증 미들웨어 구축

---
+++ middleware/authenticateUser.ts
@@ -0,0 +1,22 @@
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+export const authChecker = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split('Bearer ')[1]; //토큰만 추출
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY!, (err) => {
+      if (err) {
+        res.status(401).json({ error: '회원이 인증되지 않았습니다.' });
+      } else {
+        next(); //토큰이 정상적으로 인증되면 next()를 통해 다음 미들웨어로 넘어감
+      }
+    });
+  } else {
+    res.status(401).json({ error: '회원이 인증되지 않았습니다.' });
+  }
+};
