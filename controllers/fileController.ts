dd9f814375d5da52706db345832130373c30bac6
Minyoung Lee <zhal7779@gmail.com>
2024-11-27 01:03:59 +0900

    Feat: 인증 미들웨어 res.locals에 사용자 id 저장하도록 수정

--- controllers/fileController.ts
+++ controllers/fileController.ts
@@ -3,17 +3,11 @@ import fileService from '../services/fileService';
 
 const postFile = async (req: Request, res: Response): Promise<void> => {
   try {
-    const userId = req.user?.id; // 미들웨어에서 설정한 userId 가져오기
-    if (!userId) {
-      res.status(400).json({ message: '유효하지 않은 사용자입니다.' });
-      return;
-    }
-
+    const userId = res.locals.userId; // 미들웨어에서 설정한 userId 가져옴
     await fileService.postFile(userId);
-    res.status(200).json({ message: '파일이 생성되었습니다.' });
+    res.status(200).json('파일이 생성되었습니다.');
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: '파일 생성에 실패했습니다.' });
+    res.status(500).send({ message: '파일 생성에 실패했습니다.' });
   }
 };
 
--- middleware/authenticateUser.ts
+++ middleware/authenticateUser.ts
@@ -9,14 +9,13 @@ export const authenticateUser = (
   if (req.headers.authorization) {
     const token = req.headers.authorization.split('Bearer ')[1];
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY!, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY!, (err, decoded: any) => {
       if (err) {
         res.status(401).json({ error: '회원이 인증되지 않았습니다.' });
       } else {
-        // decoded 객체에 있는 userId 추출
-        const decodedToken = decoded as { userId: string }; // 토큰 payload 구조에 따라 변경
-        req.user = { id: decodedToken.userId }; // req.user에 userId 저장
-        next(); // 다음 미들웨어로 진행
+        // 토큰에서 추출한 유저 ID를 res.locals에 저장
+        res.locals.userId = decoded.id;
+        next();
       }
     });
   } else {
