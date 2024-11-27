284e13aab43274ee020edfbc5411b161b1a962a4
Minyoung Lee <zhal7779@gmail.com>
2024-11-28 00:44:03 +0900

    Feat: 파일 태그 업데이트 api 라우팅

--- routes/fileRoutes.ts
+++ routes/fileRoutes.ts
@@ -7,4 +7,7 @@ const router = express.Router();
 // 파일 생성
 router.post('/', authenticateUser, fileController.postFile);
 
+//파일 태그 수정
+router.patch('/update/tag', authenticateUser, fileController.patchFileTag);
+
 export default router;
