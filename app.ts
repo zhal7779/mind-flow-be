95aaa59d280cd9e62843f6b29dc9830920e6459d
sylee96 <syoon369@gmail.com>
2024-12-02 16:02:23 +0900

    Feat: cors 전체 도메인 허용

--- app.ts
+++ app.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import authRoutes from './routes/authRoutes';
-import fileRoutes from './routes/fileRoutes';
-import nodeRoutes from './routes/nodeRoutes';
-import cookieParser from 'cookie-parser';
-import './tasks/cronTasks';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import authRoutes from "./routes/authRoutes";
+import fileRoutes from "./routes/fileRoutes";
+import nodeRoutes from "./routes/nodeRoutes";
+import cookieParser from "cookie-parser";
+import "./tasks/cronTasks";
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
   credentials: true, // 자격 증명 허용 (쿠키 전송 허용)
 };
 
@@ -21,14 +21,14 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // 로그 미들웨어
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // 쿠키 파서 등록
 app.use(cookieParser());
 
 // 라우트 설정
-app.use('/api/auth', authRoutes);
-app.use('/api/file', fileRoutes);
-app.use('/api/node', nodeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/file", fileRoutes);
+app.use("/api/node", nodeRoutes);
 
-app.listen(8080, () => console.log('8080번 포트 '));
+app.listen(8080, () => console.log("8080번 포트 "));
