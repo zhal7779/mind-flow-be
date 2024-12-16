c03ebcecf0dd33bb1a72512eef316cc821ab8d19
minyoung <zhal7779@naver.com>
2024-12-16 19:42:45 +0900

    Feat: 로그아웃 라우터 처리

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import authController from '../controllers/authController';
+import express from "express";
+import authController from "../controllers/authController";
 
 const router = express.Router();
 
 // 유저 조회
-router.get('/', authController.getUser);
+router.get("/", authController.getUser);
 
 // 유저 생성
-router.post('/', authController.postUser);
+router.post("/", authController.postUser);
 
 // 로그인
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
 //로그아웃
-router.post('/logout', authController.postLogin);
+router.post("/logout", authController.postLogout);
 
 //토큰 재발급
-router.get('/refresh', authController.postRefresh);
+router.get("/refresh", authController.postRefresh);
 
 export default router;
